988295000488f3fc78a7d143c645aaec3ec948ca
signaturecoder <snkmr13@gmail.com>
2024-07-03 10:05:53 +0530

    30 days LC Challenge: minDifference: Refactor

--- Arrays/minDifference.js
+++ Arrays/minDifference.js
@@ -6,18 +6,25 @@
  * @param {number[]} nums
  * @return {number}
  * 
- * Approach: Applying Greedy Algorithm and sorting to find the optimal solution here
+ * Approach: 
+ *  Applying Greedy Algorithm and sorting to find the optimal solution here
  * Here, 
+ *  sort: 2,3,4,5       O(nlog(n)
  * 
- * // sort: 2,3,4,5
- * 
- * There are 4 possible senarios which can decrease the difference between minimum and maximum
- * a. 
+ *  There are 4 possible senarios which can decrease the difference between minimum and maximum
+ *  a. Pick last 3 largest elements,                            we left with indexs i=0, n-4 
+ *  b. Pick the first smallest and last 2 largest elements,     we left with indexs i=1, n-3 
+ *  c. Pick the first 2 smallest and last 1 largest elements,   we left with indexs i=2, n-2 
+ *  d. Pick the first 3 smallest,                               we left with indexs i=3, n-1 
+ *   
+ *  Thus, we can run the loop for the first 4 elements only and calculate the difference like
+ *  n-i  -  4-i
+ *  Math.abs(nums[n-i] - nums[4-i])
  * 
  * Instead of Infinity, we can take Number.MAX_VALUE
  */
 var minDifference = function(nums) {
-    let minDiff = Infinity, n = nums.length;
+    let minDiff = Number.MAX_VALUE, n = nums.length;
     if(n <= 4) return 0;
     nums.sort((a, b) => a - b);
     for(let i = 1; i <= 4; i++) {
